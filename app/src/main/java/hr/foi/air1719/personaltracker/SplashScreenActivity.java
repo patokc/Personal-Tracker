78eb4a0da78c8e5fae06274e02af67ab01f1a57b
Patricija <p.potrosko@gmail.com>
2017-11-14 16:35:45 +0100

    Splash Screen added, changed application icon

---
+++ app/src/main/java/hr/foi/air1719/personaltracker/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package hr.foi.air1719.personaltracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        thread.start();
+    }
+}
