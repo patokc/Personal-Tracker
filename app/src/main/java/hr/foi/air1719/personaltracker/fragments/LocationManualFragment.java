45cc63caaa52844ec739a816a58006584b60001f
Patricija <p.potrosko@gmail.com>
2017-11-22 22:34:23 +0100

    Implemented LocationManualFragment layout, added Geocoder
    Added geocoder for transforming coordinates to real location address

--- app/src/main/java/hr/foi/air1719/personaltracker/fragments/LocationManualFragment.java
+++ app/src/main/java/hr/foi/air1719/personaltracker/fragments/LocationManualFragment.java
@@ -1,11 +1,22 @@
 package hr.foi.air1719.personaltracker.fragments;
 
 import android.app.Fragment;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import hr.foi.air1719.location.IGPSActivity;
+import hr.foi.air1719.location.MyLocation;
 import hr.foi.air1719.personaltracker.R;
 
 /**
@@ -13,6 +24,8 @@ import hr.foi.air1719.personaltracker.R;
  */
 
 public class LocationManualFragment extends Fragment {
+    Geocoder geocoder ;
+    List<Address> lokacija;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.location_manual_fragment, container, false);
@@ -21,5 +34,26 @@ public class LocationManualFragment extends Fragment {
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        geocoder = new Geocoder(getActivity(), Locale.getDefault());
+
+        TextView address = (TextView) getView().findViewById(R.id.txt_address);
+        EditText note = (EditText) getView().findViewById(R.id.txt_note);
+        MyLocation myLocation = new MyLocation();
+        Location location = myLocation.GetLastKnownLocation((IGPSActivity) getActivity());
+        try {
+            lokacija = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String addressLine= lokacija.get(0).getAddressLine(0);
+
+        String city = lokacija.get(0).getAdminArea();
+
+        String finalAddress = addressLine+","+city;
+
+        address.setText(finalAddress);
+
     }
+
 }
