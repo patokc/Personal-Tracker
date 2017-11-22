918b5ad2b94ada16891e49f189c577e1894eb676
Patricija <p.potrosko@gmail.com>
2017-11-22 18:52:34 +0100

    Map fragment added
    Added class and layout for MapFragment, improved application's icon

---
+++ app/src/main/java/hr/foi/air1719/personaltracker/fragments/MapFragment.java
@@ -0,0 +1,26 @@
+package hr.foi.air1719.personaltracker.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hr.foi.air1719.personaltracker.R;
+
+/**
+ * Created by Patricija on 11/21/2017.
+ */
+
+public class MapFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.map_fragment, container, false);
+
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
