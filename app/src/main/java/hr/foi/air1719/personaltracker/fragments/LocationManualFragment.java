a9289a3be992e2faea31fb48d529ad5818217df9
Patricija <p.potrosko@gmail.com>
2017-11-23 01:43:00 +0100

    Add location's image during manual saving of location - implemented
    Added method AddImage which opens the gallery. Changes in LocationManualFragment layout - imagebutton fix. Added override method onActivityResult which is used for getting image from gallery and displaying choosen image in imagebutton.

--- app/src/main/java/hr/foi/air1719/personaltracker/fragments/LocationManualFragment.java
+++ app/src/main/java/hr/foi/air1719/personaltracker/fragments/LocationManualFragment.java
@@ -1,14 +1,20 @@
 package hr.foi.air1719.personaltracker.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -19,6 +25,8 @@ import hr.foi.air1719.location.IGPSActivity;
 import hr.foi.air1719.location.MyLocation;
 import hr.foi.air1719.personaltracker.R;
 
+import static android.app.Activity.RESULT_OK;
+
 /**
  * Created by Patricija on 11/22/2017.
  */
@@ -26,6 +34,10 @@ import hr.foi.air1719.personaltracker.R;
 public class LocationManualFragment extends Fragment {
     Geocoder geocoder ;
     List<Address> lokacija;
+    public static int RESULT_LOAD_IMAGE = 1;
+    private ImageButton outputImage;
+    Bitmap outputBitmap;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.location_manual_fragment, container, false);
@@ -39,6 +51,7 @@ public class LocationManualFragment extends Fragment {
 
         TextView address = (TextView) getView().findViewById(R.id.txt_address);
         EditText note = (EditText) getView().findViewById(R.id.txt_note);
+
         MyLocation myLocation = new MyLocation();
         Location location = myLocation.GetLastKnownLocation((IGPSActivity) getActivity());
         try {
@@ -47,13 +60,55 @@ public class LocationManualFragment extends Fragment {
             e.printStackTrace();
         }
         String addressLine= lokacija.get(0).getAddressLine(0);
-
-        String city = lokacija.get(0).getAdminArea();
-
-        String finalAddress = addressLine+","+city;
+        String county = lokacija.get(0).getAdminArea();
+        String finalAddress = addressLine+", "+county;
 
         address.setText(finalAddress);
 
+        outputImage = (ImageButton) getView().findViewById(R.id.img_location_photo);
+        outputImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddImage();
+            }
+        });
+        Button buttonSave = (Button) getView().findViewById(R.id.btn_save_location_data);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void AddImage()
+    {
+        Intent intentGalery = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intentGalery.setType("image/*");
+        startActivityForResult(intentGalery, RESULT_LOAD_IMAGE);
+    }
+
+    private void Save () {
+
+    }
+
+   @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK) {
+            if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+                Uri imageUri = data.getData();
+                outputImage.setImageURI(imageUri);
+                try {
+                    outputBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                    outputImage.setImageBitmap(outputBitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
