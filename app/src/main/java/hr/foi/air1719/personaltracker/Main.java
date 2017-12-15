59c59a29d025389b31938995526dd737348bc3d5
Nikolina Kužir <nika1011@gmail.com>
2017-12-15 09:52:12 +0100

    Running Mode fragment added to Main activity

--- app/src/main/java/hr/foi/air1719/personaltracker/Main.java
+++ app/src/main/java/hr/foi/air1719/personaltracker/Main.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import hr.foi.air1719.personaltracker.fragments.DrivingModeFragment;
+import hr.foi.air1719.personaltracker.fragments.RunningModeFragment;
 
 import hr.foi.air1719.personaltracker.fragments.MapFragment;
 import hr.foi.air1719.personaltracker.fragments.Settings;
@@ -91,6 +92,7 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
                 break;
             case R.id.runningMode:
                 tag = "RunningMode";
+                fragment=new RunningModeFragment();
                 break;
             case R.id.drivingMode:
                 fragment = new DrivingModeFragment();
--- app/src/main/java/hr/foi/air1719/personaltracker/fragments/RunningModeFragment.java
+++ app/src/main/java/hr/foi/air1719/personaltracker/fragments/RunningModeFragment.java
@@ -40,6 +40,5 @@ public class RunningModeFragment extends Fragment implements IGPSActivity
     }
 
 
-
 }
 
