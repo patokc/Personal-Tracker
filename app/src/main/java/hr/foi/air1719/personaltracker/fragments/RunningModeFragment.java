0bd5d01ff85a273728621f929d93140aeeb64926
Nikolina Kužir <nika1011@gmail.com>
2017-12-27 09:34:40 +0100

    Added saving location in database
    -added methods for start and stop Running mode
    -added method for storing location (GPS coordinates) to database in Running mode fragment

--- app/src/main/java/hr/foi/air1719/personaltracker/fragments/RunningModeFragment.java
+++ app/src/main/java/hr/foi/air1719/personaltracker/fragments/RunningModeFragment.java
@@ -6,7 +6,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import hr.foi.air1719.core.facade.DatabaseFacade;
+import hr.foi.air1719.database.entities.Activity;
+import hr.foi.air1719.database.entities.ActivityMode;
+import hr.foi.air1719.database.entities.GpsLocation;
 import hr.foi.air1719.location.IGPSActivity;
+import hr.foi.air1719.location.MyLocation;
 import hr.foi.air1719.personaltracker.R;
 
 /**
@@ -15,6 +23,10 @@ import hr.foi.air1719.personaltracker.R;
 
 public class RunningModeFragment extends Fragment implements IGPSActivity
 {
+
+    MyLocation myLocation=null;
+    Button btnRunningModeStart=null;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -25,6 +37,37 @@ public class RunningModeFragment extends Fragment implements IGPSActivity
     public void onViewCreated(View view, Bundle savedInstanceState)
     {
         super.onViewCreated(view, savedInstanceState);
+
+        btnRunningModeStart = (Button) getView().findViewById(R.id.btnRunningModeStart);
+        btnRunningModeStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                onClick_StartRunningMode(v);
+            }
+        });
+
+    }
+
+    private void onClick_StartRunningMode(View v)
+
+    {
+        if(myLocation ==null)
+        {
+            Toast.makeText(this.getActivity(), "Start Running mode", Toast.LENGTH_SHORT).show();
+            myLocation = new MyLocation();
+            myLocation.LocationStart(this);
+            btnRunningModeStart.setText("Stop");
+        }
+
+        else
+        {
+            Toast.makeText(this.getActivity(), "Stop Running mode", Toast.LENGTH_SHORT).show();
+            myLocation.LocationListenerStop();
+            myLocation = null;
+            btnRunningModeStart.setText("Start");
+        }
+
     }
 
 
@@ -37,8 +80,16 @@ public class RunningModeFragment extends Fragment implements IGPSActivity
     @Override
     public void locationChanged(Location location)
     {
+        try
+        {
+            DatabaseFacade dbf = new DatabaseFacade(getView().getContext());
+            Activity activity = new Activity(ActivityMode.RUNNING);
+            dbf.saveLocation(new GpsLocation(activity.getActivityId(), location.getLongitude(), location.getLatitude(), location.getAccuracy()));
+        }catch (Exception E)
+
+        {
+        E.printStackTrace();
+        }
     }
-
-
 }
 
