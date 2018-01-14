5dc000f83cc95ca7d9670f52f442ea5f69fb0dcf
Timotea Štrucelj <timstruce@foi.hr>
2018-01-14 17:18:13 +0100

    Account settings cancel button added
    Added cancel button for account settings

--- app/src/main/java/hr/foi/air1719/personaltracker/fragments/AccountSettingsFragment.java
+++ app/src/main/java/hr/foi/air1719/personaltracker/fragments/AccountSettingsFragment.java
@@ -1,12 +1,18 @@
 package hr.foi.air1719.personaltracker.fragments;
 
 import android.app.Fragment;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import android.support.annotation.Nullable;
+
+import android.support.design.widget.NavigationView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import hr.foi.air1719.personaltracker.R;
 
@@ -14,7 +20,7 @@ import hr.foi.air1719.personaltracker.R;
  * Created by Timotea on 13.1.2018..
  */
 
-public class AccountSettingsFragment extends Fragment {
+public class AccountSettingsFragment extends Fragment implements View.OnClickListener {
 
     public AccountSettingsFragment() {
 
@@ -22,11 +28,28 @@ public class AccountSettingsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account_settings, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_account_settings, container, false);
+        Button button = (Button) view.findViewById(R.id.actionCancel);
+        button.setOnClickListener(this);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_container, new hr.foi.air1719.personaltracker.fragments.MapFragment());
+        fragmentTransaction
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit();
+
     }
 }
