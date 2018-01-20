a27606ddf5bb3cdcb304438b944c3da426a32894
dvuk <drazen0001@gmail.com>
2018-01-20 12:26:40 +0100

    Created espresso test for "My Location"
    Created espresso test for "My Location"
    Method:
    LocationChanged()
    GetLastKnownLocation()

--- location/src/androidTest/java/hr/foi/air1719/location/MyLocationTest.java
+++ location/src/androidTest/java/hr/foi/air1719/location/MyLocationTest.java
@@ -1,15 +1,41 @@
 package hr.foi.air1719.location;
 
+import android.app.Fragment;
+import android.app.Instrumentation;
+import android.content.Context;
 import android.location.Location;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
 
-public class MyLocationTest {
+
+@RunWith(AndroidJUnit4.class)
+public class MyLocationTest extends android.app.Fragment implements IGPSActivity{
+
     @Test
-    public void GetLastKnownLocation() {
+    public void GetLastKnownLocation() throws Exception {
+
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        Fragment f = new Fragment();
+
+        MyLocation myLocation = new MyLocation();
+        Location location = myLocation.GetLastKnownLocation(this);
+
+        assertEquals("Error with location.", location, null);
+    }
+
+    @Override
+    public void locationChanged(Location location) {
 
     }
 }
