353ff193b24f07b25c646fee6c0429ac6fc48f7b
Patricija <p.potrosko@gmail.com>
2018-01-21 16:38:52 +0100

    Implemented mileageFragment
    - added graphview for distance sort by modes

--- app/src/main/java/hr/foi/air1719/personaltracker/fragments/MileageFragment.java
+++ app/src/main/java/hr/foi/air1719/personaltracker/fragments/MileageFragment.java
@@ -1,20 +1,27 @@
 package hr.foi.air1719.personaltracker.fragments;
 
 import android.app.Fragment;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LegendRenderer;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Map;
+
+import hr.foi.air1719.core.facade.DatabaseFacade;
+import hr.foi.air1719.database.entities.Activity;
 import hr.foi.air1719.personaltracker.R;
 
 /**
@@ -23,6 +30,10 @@ import hr.foi.air1719.personaltracker.R;
 
 public class MileageFragment extends Fragment {
 
+    Float zbrojDrive = 0f;
+    Float zbrojRun = 0f;
+    Float zbrojWalk = 0f;
+    List<Activity> listActivity= new ArrayList();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -32,27 +43,55 @@ public class MileageFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        fillListView();
+        BarChart barChart = (BarChart) getView().findViewById(R.id.graph);
 
-        GraphView graph = (GraphView) getView().findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
-        });
-        graph.addSeries(series);
-        graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(0);
-        graph.getViewport().setMaxY(7);
-
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(3);
+        ArrayList<BarEntry> entrie = new ArrayList<>();
+        entrie.add(new BarEntry(zbrojRun,0));
+        entrie.add(new BarEntry(zbrojDrive,1));
+        BarDataSet dataSet = new BarDataSet(entrie,"Modes");
 
 
-        series.setTitle("Running mode");
-        graph.getLegendRenderer().setVisible(true);
-        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
-
-        series.setSpacing(20);
+        ArrayList<String> date = new ArrayList<>();
+        date.add("RUNNING MODE");
+        date.add("DRIVING MODE");
+        BarData barData = new BarData(date,dataSet);
+        barChart.setData(barData);
 
     }
+
+    final Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message message) {
+            System.out.println(message.toString());
+
+            Map<String, Activity> mapList = (Map<String, Activity>)message.obj;
+            listActivity = new ArrayList();
+            listActivity.addAll(mapList.values());
+            for (Activity a : listActivity) {
+                if (a.getMode().equals("RUNNING")) {
+                    zbrojRun += a.getDistance();
+                }
+                else if(a.getMode().equals("DRIVING")){
+                    zbrojDrive +=a.getDistance();
+                }
+                else {
+                    zbrojWalk+=a.getDistance();
+                }
+
+            }
+        }
+    };
+
+    private void fillListView() {
+        new Thread(new Runnable() {
+            public void run() {
+                DatabaseFacade dbfacade = new DatabaseFacade(getView().getContext());
+                Message message = mHandler.obtainMessage(1, dbfacade.getAllActivities());
+                message.sendToTarget();
+            }
+        }).start();
+    }
+
+
 }
